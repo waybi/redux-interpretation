7bfae40d802f32fc20dc388af10f7b425aa6e501
hhk <huhhking@gmail.com>
2019-07-11 16:41:03 +0800

    注释 combineReducers

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -62,9 +62,15 @@ function getUnexpectedStateShapeWarningMessage(
   }
 }
 
+/**
+ * 这个函数是用来检验 reducer, 保证所有情况都能返回 state
+ * @param {*} reducers
+ */
 function assertReducerShape(reducers) {
   Object.keys(reducers).forEach(key => {
     const reducer = reducers[key]
+    // 如果 reducer 传的 state 是 undefined, action 是 INIT 时返回的初始 state 是 undefined 时抛出错误
+    // 初始 state 不能是 undefined，但是可以是 null
     const initialState = reducer(undefined, { type: ActionTypes.INIT })
 
     if (typeof initialState === 'undefined') {
@@ -77,6 +83,7 @@ function assertReducerShape(reducers) {
       )
     }
 
+    // 测试传不存在的 action 时的情况，保证所有情况都能返回 state
     if (
       typeof reducer(undefined, {
         type: ActionTypes.PROBE_UNKNOWN_ACTION()
@@ -102,6 +109,9 @@ function assertReducerShape(reducers) {
  * into a single state object, whose keys correspond to the keys of the passed
  * reducer functions.
  *
+ * 这个函数的作用是，把一个包含各个 reducer 函数的对象，合并成一个 reducer 函数。
+ * 会执行每一个 reducer，并把它们的结果（各个 state）合并成一个 state 对象，对象的 key 就是传给 reducer 的参数的 key
+ *
  * @param {Object} reducers An object whose values correspond to different
  * reducer functions that need to be combined into one. One handy way to obtain
  * it is to use ES6 `import * as reducers` syntax. The reducers may never return
@@ -113,17 +123,23 @@ function assertReducerShape(reducers) {
  * passed object, and builds a state object with the same shape.
  */
 export default function combineReducers(reducers) {
+  // 获取 reducers 这个对象的建 keys 对象
   const reducerKeys = Object.keys(reducers)
   const finalReducers = {}
   for (let i = 0; i < reducerKeys.length; i++) {
     const key = reducerKeys[i]
 
+    // 判断 key 对应的 reducer 是不是 `undefined`
     if (process.env.NODE_ENV !== 'production') {
       if (typeof reducers[key] === 'undefined') {
         warning(`No reducer provided for key "${key}"`)
       }
     }
-
+    /**
+     * 确保 reducer 是个函数，然后根据 key/value 存在新对象 finalReducers
+     * 这个新对象，包含的是过滤 undefined 和 非函数 后的 reducer
+     * 保存在另一个对象，可以再后续的修改中，不影响到原来的 reducers 这个对象
+     */
     if (typeof reducers[key] === 'function') {
       finalReducers[key] = reducers[key]
     }
@@ -144,6 +160,7 @@ export default function combineReducers(reducers) {
     shapeAssertionError = e
   }
 
+  // 返回一个合并后的 reducer 函数, state 默认值为空对象
   return function combination(state = {}, action) {
     if (shapeAssertionError) {
       throw shapeAssertionError
@@ -163,11 +180,13 @@ export default function combineReducers(reducers) {
 
     let hasChanged = false
     const nextState = {}
+    // 循环执行 reducer，这部分写的很清晰
     for (let i = 0; i < finalReducerKeys.length; i++) {
       const key = finalReducerKeys[i]
       const reducer = finalReducers[key]
       const previousStateForKey = state[key]
       const nextStateForKey = reducer(previousStateForKey, action)
+      // 这里验证是否会没有 state 返回
       if (typeof nextStateForKey === 'undefined') {
         const errorMessage = getUndefinedStateErrorMessage(key, action)
         throw new Error(errorMessage)
@@ -175,6 +194,7 @@ export default function combineReducers(reducers) {
       nextState[key] = nextStateForKey
       hasChanged = hasChanged || nextStateForKey !== previousStateForKey
     }
+    // state 有改变返回下一个 state，否则返回原来的 state
     return hasChanged ? nextState : state
   }
 }
